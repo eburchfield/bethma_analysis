7493570cda2515d5870b072acba34b2c61cd93c4
Emily Burchfield <emilykayburchfield@gmail.com>
2016-01-22 17:30:46 -0600

    update bethma_vi and others

--- bethma_logit_vi.R
+++ bethma_logit_vi.R
@@ -1,7 +1,6 @@
 require(dplyr)
 require(gdata)
 require(ggplot2)
-require(lme4)
 require(rjags)
 require(ggmcmc)
 require(string)
@@ -11,18 +10,23 @@ require(arm)
 require(shinystan)
 require(stats)
 
-#add cauchy prior
+#kruschke says standardize predictors to reduce AC in MCMC sampling
+#interactions
+#gelman 381
+#change in n chains, n adapt changes results
 
+#intercept is male minor minority farmer who doesn't own land or an agrowell, 
+#doesn't cultivate in the HE or major systems, and doesn't participate in FO
+
+#...of individuals that have heard of bethma...
 #data setup
-y <- c1$ADP1_F1
-#yes = 1; no = 2
+df <- c1[!is.na(c1$ADP1_B1),]
+y <- df$ADP1_B1
 y <-ifelse(y==1, 1, 0)
-vname <- "Have you ever practiced bethma?"
-it <- c("Minor", "Major")
 n <- length(y)
+
 dc.names <- as.vector(c1$HI4)
 uq <- unique(dc.names)
-itc <- c(0,1,0,1,1,0,0,0,0,1,0,0,1,0,1)
 n.dc <- length(uq)
 dc <- rep(NA, n.dc)
 for (i in 1:n.dc){
@@ -32,90 +36,110 @@ for (i in 1:n.dc){
 
 
 model_string <- "model{
-#level-1 likelihood
+
 for (i in 1:n){
-y[i] ~ dbin(mu[i], 1) 
-p.bound[i] <- max(0, min(1, mu[i])) #381 gelman
-logit(mu[i]) <- a[dc[i]] + b1*x1[i] + b2*x2[i] + b3*x3[i] + b4*x4[i]
-+ b5*x5[i] + b6*x6[i] + b7*x7[i] + b8*x8[i]
+  y[i] ~ dbern(p[i])
+  #dbin(p[i], 1)
+  #p[i] <- max(0, min(1, yhat[i])) #bounded probability
+  logit(p[i]) <- a[dc[i]] + b1*x1[i] + b2*x2[i] + b3*x3[i] + b4*x4[i]
+  + b5*x5[i] + b6*x6[i] + b7*x7[i] + b8*x8[i]
 }
 
-#if any additional priors in likelihood of y[i], specify here
-b1 ~ dnorm(0,.001) #could do prior for spread
-b2 ~ dnorm(0,.001) #instead of fixing variance, could make tau.b, etc... to allow for correlatoin
-b3 ~ dnorm(0,.001) 
-b4 ~ dnorm(0,.001) 
-b5 ~ dnorm(0,.001) 
-b6 ~ dnorm(0,.001) 
-b7 ~ dnorm(0,.001) 
-b8 ~ dnorm(0,.001) 
-#level-2 likelihood
+b1 ~ dnorm(0,.0001) 
+b2 ~ dnorm(0,.0001) 
+b3 ~ dnorm(0,.0001) 
+b4 ~ dnorm(0,.0001) 
+b5 ~ dnorm(0,.0001) 
+b6 ~ dnorm(0,.0001) 
+b7 ~ dnorm(0,.0001) 
+b8 ~ dnorm(0,.0001) 
+
 for (j in 1: n.dc ){
-a[j] ~ dnorm(g0, tau.a)  #not goj, g1j
+a[j] ~ dnorm(mu.a, tau.a)  
 }
-#level-3 hyperlevel (SL)
-g0 ~ dnorm(0,.001) 
-tau.a <- pow(sigma.a , -2)
-sigma.a ~ dnorm(0,.001) 
 
+mu.a ~ dunif(0,100)
+tau.a <- pow(sigma.a , -2)  #use half-normal when # groups small
+sigma.a ~ dunif (0, 100)  #Gelman (2006)
 }"
 
-
+#dnorm(0,.0001) | (0,1) creates half normla prior
 
 #initialize variables
 inits <- function(chain) {
   list (a=rnorm(n.dc), b1 = rnorm(1), b2 = rnorm(1),
         b3 = rnorm(1), b4 = rnorm(1), b5 = rnorm(1),
         b6 = rnorm(1), b7 = rnorm(1), b8 = rnorm(1),
-        g0 = rnorm(1), sigma.a = runif(1)) }
+        mu.a = runif(1), sigma.a = runif(1)) }
 
 #create dataframe
 data <- list(n = n, n.dc = n.dc, y = y, dc = dc, 
-             #individual level
              x1 = c1$agrowell_user, x2 = c1$major_flag,
              x3 = c1$female, x4 = c1$sinhalese,
              x5 = c1$Standardized_SES, x6 = c1$owner, 
              x7 = c1$head_end, x8 = c1$fo)
 
 #tell JAGS parameters to report back
-parameters <- c("a", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "g0", "sigma.a")
+parameters <- c("a", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "mu.a", "sigma.a")
 
 #compile jags model
 model <- jags.model(textConnection(model_string),
+                    data = data, 
+                    inits = inits,
+                    n.chains = 5,
+                    n.adapt = 500)
+
+#take 2000 random samples of each of the 3 chains
+update(model, n.iter = 7000)
+model_outcome <- coda.samples(model, variable.names = parameters, n.iter = 7000)
+my_sso <- as.shinystan(model_outcome)
+my_sso <- launch_shinystan(my_sso)
+
+#test model fit
+M1 = lmer(y ~ agrowell_user + major_flag + female + sinhalese + Standardized_SES +
+          owner + head_end + fo + (1|HI4), data = c1, REML = "False", family = binomial(link = "logit")) 
+
+b1.t <- fixef(M1)[2]
+b2.t <- fixef(M1)[3]
+b3.t <- fixef(M1)[4]
+b4.t <- fixef(M1)[5]
+b5.t <- fixef(M1)[6]
+b6.t <- fixef(M1)[7]
+b7.t <- fixef(M1)[8]
+b8.t <- fixef(M1)[9]
+mu.a.t <- fixef(M1)[1]
+sigma.a.t <- 1.5 #random
+
+a.t <- rep(NA, n.dc)
+for (j in 1:n.dc){
+  a.t[j] <- rnorm(1, mu.a.t, sigma.a.t)
+}
+
+y.fake <- rep(NA, n)
+for (i in 1:n) {
+  y.fake[i] <- rnorm(1, a.t[dc[i]] + b1.t*c1$agrowell_user[i] + b2.t*c1$major_flag[i] + b3.t*c1$female[i] + b4.t*c1$sinhalese[i] + 
+    b5.t*c1$Standardized_SES[i] + b6.t*c1$owner[i] + b7.t*c1$head_end[i] + b8.t*c1$fo[i], sigma.a.t)
+}
+
+data.fake <- list(n = n, n.dc = n.dc, y = y.fake, dc = dc, 
+             x1 = c1$agrowell_user, x2 = c1$major_flag,
+             x3 = c1$female, x4 = c1$sinhalese,
+             x5 = c1$Standardized_SES, x6 = c1$owner, 
+             x7 = c1$head_end, x8 = c1$fo)
+
+#tell JAGS parameters to report back
+parameters.fake <- c("a", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "mu.a", "sigma.a")
+
+#compile jags model
+model.fake <- jags.model(textConnection(model_string),
                     data = data, 
                     inits = inits,
                     n.chains = 3,
                     n.adapt = 100)
 
 #take 2000 random samples of each of the 3 chains
-update(model, n.iter = 5000)
-model_outcome <- coda.samples(model, variable.names = parameters, n.iter = 5000)
-my_sso <- as.shinystan(model_outcome)
+update(model.fake, n.iter = 3000)
+model_outcome_fake <- coda.samples(model.fake, variable.names = parameters.fake, n.iter = 3000)
+my_sso <- as.shinystan(model_outcome_fake)
 my_sso <- launch_shinystan(my_sso)
-
-
-#diagnosing mixing of chains, we want good overlap of chains
-samples <- ggs(model_outcome, family = '(sigma|b).*')
-ggs_traceplot(samples) + theme_bw() + theme(legend.position='none', strip.background = element_blank())
-
-#diagnosing aucotorrelation
-auto.plot <- ggs_autocorrelation(samples, family = "sigma.a") +
-  theme_bw() + theme(legend.position = 'none', strip.background = element_blank())
-auto.plot
-
-#if we see autocorrelation, we can thin the MC by telling it to remember only every fourth iteration
-# thin.steps = 4
-# model_outcome_ac <- coda.samples(model_outcome, variable.names = parameters,
-#                                 n.iter = 2000, thin = thin.steps)
-# auto.plot.thinned <- ggs_autocorrelation(ggs(model_outcome_ac), family = 'sigma.a') +
-#   theme_bw() + 
-#   theme(legend.position='none', strip.background = element_blank())
-# print(auto.plot)
-# print(auto.plot.thinned)
-
-#gelman-rubin scale reduction factor (how much better would predictions be with infinite number of iterations)
-gelman.diag(model_outcome)
-
-
-traceplot(model_outcome)
 
--- bethma_logit_vi_cauchy.R
+++ bethma_logit_vi_cauchy.R
@@ -9,14 +9,12 @@ require(BEST)
 require(foreign)
 require(arm)
 require(shinystan)
+require(stats)
 
 #add cauchy prior
-#probably drop ui
-#include other relevant variables, build full model 
-#p. 375 GELMAN
 
 #data setup
-y <- c1$ADP1_B1
+y <- c1$ADP1_F1
 #yes = 1; no = 2
 y <-ifelse(y==1, 1, 0)
 vname <- "Have you ever practiced bethma?"
@@ -38,63 +36,55 @@ model_string <- "model{
 for (i in 1:n){
 y[i] ~ dbin(mu[i], 1) 
 p.bound[i] <- max(0, min(1, mu[i])) #381 gelman
-logit(mu[i]) <- a[dc[i]] + b1[dc[i]]*x1[i] + b2[dc[i]]*x2[i] + b3[dc[i]]*x3[i] + b4[dc[i]]*x4[i] +
-b5[dc[i]]*x5[i] + b6[dc[i]]*x6[i] + b7[dc[i]]*x7[i] + b8[dc[i]]*x8[i]
+logit(mu[i]) <- a[dc[i]] + b1*x1[i] + b2*x2[i] + b3*x3[i] + b4*x4[i]
++ b5*x5[i] + b6*x6[i] + b7*x7[i] + b8*x8[i]
 }
 
 #if any additional priors in likelihood of y[i], specify here
-
+b1 ~ dt(0,.1, 1) 
+b2 ~ dt(0,.1, 1) 
+b3 ~ dt(0,.1, 1) 
+b4 ~ dt(0,.1, 1) 
+b5 ~ dt(0,.1, 1) 
+b6 ~ dt(0,.1, 1) 
+b7 ~ dt(0,.1, 1) 
+b8 ~ dt(0,.1, 1) 
 #level-2 likelihood
 for (j in 1: n.dc ){
-a[j] ~ dnorm(g0, tau.a)  #not goj, g1j
-b1[j] ~ dnorm(b01, .001) #could do prior for spread
-b2[j] ~ dnorm(b02, .001)  #instead of fixing variance, could make tau.b, etc... to allow for correlatoin
-b3[j] ~ dnorm(b03, .001)
-b4[j] ~ dnorm(b04, .001)
-b5[j] ~ dnorm(b05, .001)
-b6[j] ~ dnorm(b06, .001)
-b7[j] ~ dnorm(b07, .001)
-b8[j] ~ dnorm(b08, .001)
-
+a[j] ~ dt(g0, tau.a, 1)  
 }
 #level-3 hyperlevel (SL)
-g0 ~ dnorm(0, .001)
-b01 ~ dnorm(0, .001)
-b02 ~ dnorm(0, .001)
-b03 ~ dnorm(0, .001)
-b04 ~ dnorm(0, .001)
-b05 ~ dnorm(0, .001)
-b06 ~ dnorm(0, .001)
-b07 ~ dnorm(0, .001)
-b08 ~ dnorm(0, .001)
+g0 ~ dt(0, .001, 1) 
 tau.a <- pow(sigma.a , -2)
-sigma.a ~ dunif (0, 100)  #cauchy
-
+sigma.a ~ dunif (0, 100)  
 }"
+
+
 
 #initialize variables
 inits <- function(chain) {
-  list (a=rnorm(n.dc), b01 = rnorm(1), b02 = rnorm(1),
-        b03 = rnorm(1), b04 = rnorm(1), b05 = rnorm(1),
-        b06 = rnorm(1), b07 = rnorm(1), b08 = rnorm(1),
+  list (a=rnorm(n.dc), b1 = rnorm(1), b2 = rnorm(1),
+        b3 = rnorm(1), b4 = rnorm(1), b5 = rnorm(1),
+        b6 = rnorm(1), b7 = rnorm(1), b8 = rnorm(1),
         g0 = rnorm(1), sigma.a = runif(1)) }
 
 #create dataframe
 data <- list(n = n, n.dc = n.dc, y = y, dc = dc, 
+             #individual level
              x1 = c1$agrowell_user, x2 = c1$major_flag,
              x3 = c1$female, x4 = c1$sinhalese,
              x5 = c1$Standardized_SES, x6 = c1$owner, 
              x7 = c1$head_end, x8 = c1$fo)
 
 #tell JAGS parameters to report back
-parameters <- c("a", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "b01", "b02", "b03", "b04", "b05", "b06", "b07", "b08", "g0", "sigma.a")
+parameters <- c("a", "b1", "b2", "b3", "b4", "b5", "b6", "b7", "b8", "g0", "sigma.a")
 
 #compile jags model
 model <- jags.model(textConnection(model_string),
-                              data = data, 
-                              inits = inits,
-                              n.chains = 3,
-                              n.adapt = 100)
+                    data = data, 
+                    inits = inits,
+                    n.chains = 3,
+                    n.adapt = 100)
 
 #take 2000 random samples of each of the 3 chains
 update(model, n.iter = 5000)
