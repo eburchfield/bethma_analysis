0f9a249fb6963e7eaeb2224e2521eda84a8b8e7a
Emily Burchfield <emilykayburchfield@gmail.com>
2016-02-16 15:31:44 -0600

    code updates

--- bethma_plot.R
+++ bethma_plot.R
@@ -14,12 +14,12 @@ require(foreign)
 #}
 
 #data
-y <- c1$ADP1_E1
-vname <- "Is bethma effective?"
+y <- c1$FAR1D_20
+vname <- "OFC planted in Yala?"
 it <- c("Minor", "Major")
 
 #histograms
-G1_1 <- c1 %>% group_by(irrigtype, ADP1_G1_1) %>% summarise(n = n()) %>% mutate(freq = n/sum(n))
+G1_1 <- c1 %>% group_by(irrigtype, FAR1D_20) %>% summarise(n = n()) %>% mutate(freq = n/sum(n))
 
 G1_1$irrigtype[G1_1$irrigtype==0] <- "Minor"
 G1_1$irrigtype[G1_1$irrigtype == 1] <- "Major"
@@ -28,15 +28,21 @@ G1_1$irrigtype[G1_1$irrigtype == 1] <- "Major"
 #G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 2] <- "A little"
 #G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 3] <- "Moderately"
 #G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 4] <- "Very"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 1] <- "Time"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 2] <- "Money"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 3] <- "Labor"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 4] <- "Not effective"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 5] <- "Low yield"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 6] <- "Knowledge"
-G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 7] <- "Other"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 1] <- "Time"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 2] <- "Money"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 3] <- "Labor"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 4] <- "Not effective"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 5] <- "Low yield"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 6] <- "Knowledge"
+# G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 7] <- "Other"
 
-#D
+
+G1_1$FAR1D_20[G1_1$FAR1D_20 == 1] <- "Yes"
+G1_1$FAR1D_20[G1_1$FAR1D_20 == 2] <- "No"
+G1_1$FAR1D_20[G1_1$FAR1D_20 == 997] <- "Yes, annual"
+G1_1$FAR1D_20[G1_1$FAR1D_20 == 9997] <- "Yes, depends on season"
+
+
 # G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 1] <- "Income"
 # G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 2] <- "Water scarcity"
 # G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 3] <- "Expert advice"
@@ -45,7 +51,7 @@ G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 7] <- "Other"
 # G1_1$ADP1_G1_1[G1_1$ADP1_G1_1 == 6] <- "Other"
 
 
-bar1 <- ggplot(G1_1, aes(x = ADP1_G1_1, y = freq, fill = irrigtype)) +
+bar1 <- ggplot(G1_1, aes(x = c1$FAR1D_20, y = freq, fill = irrigtype)) +
   labs(x = "", y = "Percent", title = vname) +
   geom_bar(position = "dodge", stat = "identity") +
   theme(legend.title = element_blank())
